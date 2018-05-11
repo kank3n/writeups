43bfa4032023ff50dcba96ce56aedc5733f25e76
kank3n <k_link_k@yahoo.co.jp>
2018-05-12 08:39:26 +0900

    Update exp_template.py

--- exp_template.py
+++ exp_template.py
@@ -33,3 +33,16 @@ if __name__ == '__main__':
     else:
         s, f = sock("localhost", 10001)
         main()
+        libc_start_offset = 0x0000000000021e50
+        system_offset = 0x0000000000046590
+        binsh_offset = 0x180543
+        one_rce_offset = 0x4647c
+        one_rce_offset = 0xe9415
+        #one_rce_offset = 0xea36d
+        fread_offset = 0x000000000006e7e0
+        stderr_offset = 0x00000000003c31c0
+        stdout_offset = 0x00000000003c3400
+        stdin_offset = 0x00000000003c3640
+        free_hook_offset = 0x00000000003c4a10
+        malloc_hook_offset = 0x00000000003c2740
+        io_list_all_offset = 0x00000000003c31a0
